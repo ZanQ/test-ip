1ad0d29b13db2c129fcd38b1205ac3cb0ac38e9d
Peter Tang <peter.tang.lai@gmail.com>
2020-09-12 20:35:51 -0600

    Fixed API test error

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,17 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+/*export default (req, res) => {
   res.statusCode = 200
   res.json({ name: 'John Doe' })
+}*/
+
+export default function handle(req, res) {
+
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+      if (ip.substr(0, 7) == "::ffff:") {
+              ip = ip.substr(7)
+  }
+
+  //res.send(ip)
+  res.json({ clientIP : ip })
 }
